41d4e9350858fece5b9700bcfda5efe6ab5dd1e7
jehyun Park <adam991217@naver.com>
2019-11-18 21:13:22 +0900

    revalue table that named notice

--- QUERY_CONSTRUCT/gammaset.sql
+++ QUERY_CONSTRUCT/gammaset.sql
@@ -36,6 +36,7 @@ DROP TABLE IF EXISTS Actor;
 DROP TABLE IF EXISTS Customer_rank;
 DROP TABLE IF EXISTS Coupon;
 DROP TABLE IF EXISTS Department;
+DROP TABLE IF EXISTS Notice;
 SET foreign_key_checks = 1;
 
 CREATE TABLE Cinema (
@@ -383,4 +384,14 @@ CREATE TABLE Schedule_seat (
     REFERENCES Seat(seat_number) ON UPDATE CASCADE ON DELETE RESTRICT,
     CONSTRAINT FK_Schedule_seat_6 FOREIGN KEY(ticket_id)
     REFERENCES Ticketing_info(ticket_id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE NOTICE(
+  notice_id INT unsigned  auto_increment not NULL,
+  cinema_id INT unsigned not null,
+  content text(255) not null,
+  notice_date timestamp not NULL default current_timestamp,
+  CONSTRAINT PK_NOTICE PRIMARY KEY(notice_id),
+  CONSTRAINT FK_NOTICE FOREIGN KEY(cinema_id)
+  REFERENCES Cinema(cinema_id) ON UPDATE CASCADE ON DELETE RESTRICT
 );
