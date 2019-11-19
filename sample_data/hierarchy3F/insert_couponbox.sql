85f0c7fa1fbc7b516a70cbd40aba792f2ee86329
jehyun Park <adam991217@naver.com>
2019-11-20 00:46:08 +0900

    change coupon_box

--- sample_data/hierarchy3F/insert_couponbox.sql
+++ sample_data/hierarchy3F/insert_couponbox.sql
@@ -1,5 +1,5 @@
-insert into coupon_box values(coupon_number,customer_id,expirate_date) values
-(4,"dkssud",now()),
-(44,"dkssud",now()),
-(444,"dkssud",now()),
-(4444,"dkssud",now());
+insert into coupon_box values(coupon_number,customer_id,coupon_name,expirate_date) values
+(4,"dkssud","쓰는사람이있을카쿠폰",now()),
+(44,"dkssud","쓰는사람이있을카쿠폰",now()),
+(444,"dkssud","쓰는사람이있을카쿠폰",now()),
+(4444,"dkssud","쓰는사람이있을카쿠폰",now());
