4c9ea1e702e0536ed984476a5d76db30b692fcfb
Kim-Woojeong <43311098+Kim-Woojeong@users.noreply.github.com>
2019-11-19 13:30:56 +0900

    Add files via upload

---
+++ sample data/insertDirector.sql
@@ -0,0 +1,3 @@
+insert into Director values(37001, "게리 로스", "Gary Ross", "미국", "https://ko.wikipedia.org/wiki/%EA%B2%8C%EB%A6%AC_%EB%A1%9C%EC%8A%A4","연출자 소개를 작성해주세요");
+insert into Director values(37002, "백종열", "Baek Jongyeol", "한국", "https://movie.daum.net/person/main?personId=394220","연출자 소개를 작성해주세요");
+insert into Director values(37003, "앤드루 스탠턴", "Andrew Stanton", "미국", "https://movie.daum.net/person/main?personId=30667","연출자 소개를 작성해주세요");
