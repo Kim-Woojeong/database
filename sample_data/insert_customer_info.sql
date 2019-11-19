fff3b8079da8de76d1e6a1a560ce50b4f4b6381f
Dong Joon Lee <dongjoon0124@gmail.com>
2019-11-19 22:11:40 +0900

    Upload sample data about customer information.

---
+++ sample_data/insert_customer_info.sql
@@ -0,0 +1,18 @@
+insert into Customer_info(customer_id, password, customer_name, gender, road_address, detail_address, customer_hp, email_address, rank_name, birth_date) values
+("dkssud", "12345", "가나다", 1, "경기도 안산시 상록구 한양대학로 55(사동)", "행복관(기숙사) 000호", "01000000000", "안녕하세요@안녕.com", "SVIP", "2000-01-01"),
+("gktpdy", "678910", "라마바", 2, "경기도 안산시 상록구 한양대학로 55(사동)", "행복관(기숙사) 456호", "01023451234", "잘가요@잘가.com", "VIP", "1990-01-23"),
+("minjaejoa", "rkskekf", "김이박", 2, "경기도 안산시 상록구 한양대학로 55(사동)", "창의관(기숙사) 123호", "01011112222", "디비@디비.com", "SVIP", "1999-01-10");
+
+
+고객ID(char20)
+가입날짜(timestamp)
+pw(varchar60)
+이름(char20)
+성별(tinyint1)
+도로명주소(varchar60)
+상세주소(varchar(180))
+연락처(핸드폰)(char11))
+이메일(char30)
+등급(varchar10)
+출생년도(year)
+출생월&일 (date)
