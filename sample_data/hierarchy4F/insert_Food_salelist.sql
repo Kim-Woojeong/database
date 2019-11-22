3da7b50c78641d020292776e0233abada6289d59
Dong Joon Lee <dongjoon0124@gmail.com>
2019-11-22 22:36:15 +0900

    Update sample data about Food_salelist

---
+++ sample_data/hierarchy4F/insert_Food_salelist.sql
@@ -0,0 +1,8 @@
+﻿insert into Food_salelist(payment, coupon_number, coupon_name, customer_id) values
+(15000, NULL, NULL, NULL),
+(5700, 44, "쓰는사람이있을카쿠폰", "dkssud"),
+(23400, 4, "쓰는사람이있을카쿠폰", "dkssud"),
+(12500, 444, "쓰는사람이있을카쿠폰", "dkssud"),
+(1500, 4444, "쓰는사람이있을카쿠폰", "dkssud"),
+(9000,NULL,NULL,"dkssud")
+;
