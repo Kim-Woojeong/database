7840aa4213e0b2ff50c64024b613cac575b3039e
Dong Joon Lee <dongjoon0124@gmail.com>
2019-11-24 14:29:18 +0900

    insert sample data about Notice(2F), Purchase_list(5F), Schedule_seat(5F), and modify some wrong examples on sample data. Please apply all sample data 'again'.

--- sample_data/hierarchy4F/insert_Food_salelist.sql
+++ sample_data/hierarchy4F/insert_Food_salelist.sql
@@ -1,5 +1,5 @@
 ﻿insert into Food_salelist(payment, coupon_number, coupon_name, customer_id) values
-(15000, NULL, NULL, NULL),
+(11900, NULL, NULL, NULL),
 (5700, 44, "쓰는사람이있을카쿠폰", "dkssud"),
 (23400, 4, "쓰는사람이있을카쿠폰", "dkssud"),
 (12500, 444, "쓰는사람이있을카쿠폰", "dkssud"),
