305bba52dadbb76fcdde0d9076bf3d50ad6bb3f2
jehyun Park <adam991217@naver.com>
2019-12-08 00:09:47 +0900

    시작적인 구매창

--- webpage/component/purchase/js/purchase.js
+++ webpage/component/purchase/js/purchase.js
@@ -1,5 +1,8 @@
 window.onload = function() {
     sectionselect('cinema');
+    $('radiomovie').onclick = function(){
+        alert  ('hello');
+    }
 };
 
 // function areaselect(Type){
@@ -13,6 +16,7 @@ window.onload = function() {
 // }
 
 function changecinema() {
+    $('MENU1').style.backgroundColor = 'transparent';
     new Ajax.Request("movie_json.php",{
         method : "get",
         onSuccess : movie_JSON,
@@ -35,33 +39,35 @@ function movie_JSON(ajax) {
             paragraph.innerHTML = "상영중인 영화가 없습니다."
             $("movies").appendChild(paragraph);
         }
-        else
+        else{
+            $('MENU1').style.backgroundColor = 'pink';
             for(var j =0; j< data.movies[i].movie.length;j++){
-            var div = document.createElement("label");
-            div.id = 'POSTER'+j;
-            $("movies").appendChild(div);
-            var paragraph = document.createElement("p");        
-            paragraph.innerHTML = data.movies[i].movie[j][1];
-            var image = document.createElement("img");
-            image.src = '../img/movie/movie_'+data.movies[i].movie[j][0]+'.jpeg';
-            $('POSTER'+j).appendChild(image);
-            $('POSTER'+j).appendChild(createRadioElement('movie',data.movies[i].movie[j][0], 0));
-            $('POSTER'+j).appendChild(paragraph);
+                var div = document.createElement("label");
+                div.id = 'POSTER'+j;
+                $("movies").appendChild(div);
+                var paragraph = document.createElement("p");        
+                paragraph.innerHTML = data.movies[i].movie[j][1];
+                var image = document.createElement("img");
+                image.src = '../img/movie/movie_'+data.movies[i].movie[j][0]+'.jpeg';
+                var radio = document.createElement('input');
+                radio.type = 'radio';
+                radio.name = 'movie';
+                radio.value = data.movies[i].movie[j][0];
+                //죄송합니다. id를 이따구로 사용하면 안되는데 너무 귀찮아요.
+                radio.id = data.movies[i].movie[j][1];
+                radio.onclick = changemovie;
+                $('POSTER'+j).appendChild(image);
+                $('POSTER'+j).appendChild(radio);
+                $('POSTER'+j).appendChild(paragraph);
+            }
         }
     }
 }
 
-function createRadioElement(name, value, checked) {
-    var radioHtml = '<input type="radio" name="' + name + '" value="' + value + '"';
-    if ( checked ) {
-        radioHtml += ' checked="checked"';
-    }
-    radioHtml += '/>"' + value +'"';
-
-    var radioFragment = document.createElement('div');
-    radioFragment.innerHTML = radioHtml;
-
-    return radioFragment.firstChild;
+function changemovie(){
+    $('MENU2').style.backgroundColor = 'pink';
+    alert("선택하신 영화는" + this.id + "입니다.");
+    $('selection_movie').innerHTML = '선택하신 영화 : ' + this.id;
 }
 
 function ajaxFailed(ajax, exception) {
--- webpage/component/purchase/purchase.php
+++ webpage/component/purchase/purchase.php
@@ -30,11 +30,11 @@
 		<!-- menu bar -->
 		<menu role="role">
 			<p class="menu_name">MENU</p>
-			<button class="menu_button" id="" onclick="sectionselect('cinema')">영화관선택</button>
-			<button class="menu_button" id="" onclick="sectionselect('movie')">영화선택</button>
-			<button class="menu_button" id="" onclick="sectionselect('date')">날짜선택</button>
-			<button class="menu_button" id="" onclick="sectionselect('seat')">좌석선택</button>
-			<button class="menu_button" id="" onclick="sectionselect('approval')">결제</button>
+			<button class="menu_button" id="MENU1" onclick="sectionselect('cinema')">영화관선택</button>
+			<button class="menu_button" id="MENU2" onclick="sectionselect('movie')">영화선택</button>
+			<button class="menu_button" id="MENU3" onclick="sectionselect('date')">날짜선택</button>
+			<button class="menu_button" id="MENU4" onclick="sectionselect('seat')">좌석선택</button>
+			<button class="menu_button" id="MENU5" onclick="sectionselect('approval')">결제</button>
 		</menu>
 
 		<!-- 다이나믹 섹션 -->
@@ -79,6 +79,7 @@
 				<section id="movie">
 					<fieldset>
 						<legend><h2>영화선택</h2></legend>
+						<h3 id="selection_movie">선택하신 영화 : 미선택</h3>
 						<div id="movies">
 							<p>영화를 선택할 수 없습니다.</p>
 						</div>
