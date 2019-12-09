3bce34e689c19e4eb8390dc40ff08572013c58e0
jehyun Park <adam991217@naver.com>
2019-12-09 17:38:42 +0900

    update mainpage

--- webpage/component/common/cinema_test.php
+++ webpage/component/common/cinema_test.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8" />
   <link href="../../styles/common.css" type="text/css" rel="stylesheet" />
   <link href="../../styles/cinema_test_style.css" type="text/css" rel="stylesheet" />
+  <script type="text/javascript" src="js/cinema_test.js"></script>
   <title>zxCINEMAxz</title>
 </head>
 
@@ -21,9 +22,15 @@
   <?php
   include "../common/navigator.php";
   ?>
-  <div class="ad">
-  </div> <!-- ad -->
-
+  <?php
+  for ($i=1; $i <7 ; $i++) { 
+    ?>
+    <div class="ad">
+      <div class="mySlides fade">
+        <img src="../img/advertisement/ad<?=$i?>.png" />
+      </div>
+    </div> <!-- ad -->
+  <?php } ?>
   <div class="container">
     <div class="contents">
       <div class="contents_1">
